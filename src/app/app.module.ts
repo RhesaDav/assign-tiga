45cbfc85d9bf418a34ef6ebe934428be269c1bc6
Rhesa Davinanto <72176512+RhesaDav@users.noreply.github.com>
2021-12-08 16:18:39 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,60 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProductdetailComponent } from './productdetail/productdetail.component';
+
+import { authInterceptorProvider } from './_helpers/auth.interceptor';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PagenotfoundComponent,
+    ProductComponent,
+    CartComponent,
+    ContactComponent,
+    ProfileComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProductdetailComponent,
+    BoardUserComponent,
+    BoardAdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: 'product', component: ProductComponent},
+      {path: 'cart', component: CartComponent},
+      {path: 'profile', component: ProfileComponent},
+      {path: 'profile/register', component: RegisterComponent},
+      {path: 'profile/login', component: LoginComponent},
+      {path: 'contact', component: ContactComponent},
+      {path: 'productdetail', component: ProductdetailComponent},
+  
+      {path: '**', component: PagenotfoundComponent }
+    ])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [authInterceptorProvider],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
